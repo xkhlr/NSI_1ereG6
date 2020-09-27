67a48a052a84c8901dc153d1ff05a27c8c99e6b2
HenraL <37299738+HenraL@users.noreply.github.com>
2020-09-27 21:39:16 +0200

    updated a few old programs

--- files/programme python/blocks ex 21 p 30/Blocks.py
+++ files/programme python/blocks ex 21 p 30/Blocks.py
@@ -1,9 +1,17 @@
 from time import sleep
+import datetime
+date=datetime.datetime.now()
+DATE="{}_{}_{}".format(date.day,date.month,date.year)
+def write(a):
+    f=open("Blocks_data_{}.txt".format(DATE),"a")
+    f.write(a)
+    f.close()
 blockmax=int(input("Entrez le nombre de bloc disponible:"))
 block=1
 level=1
 while block<blockmax:
     print("niveau {} : nombre de block {}".format(level,block))
+    write("niveau {} : nombre de block {}\n".format(level,block))
     block+=3
     level+=1
 sleep(2)
--- files/programme python/blocks ex 21 p 30/Problème2, Partie B, Suites arithmétiques, b) suites.py
+++ files/programme python/blocks ex 21 p 30/Problème2, Partie B, Suites arithmétiques, b) suites.py
@@ -1,13 +1,25 @@
+import datetime
+date=datetime.datetime.now()
+DATE="{}_{}_{}".format(date.day,date.month,date.year)
+def write(a):
+    f=open("Problème2_Partie_B_Suites_arithmétiques_b)_suites_data_{}.txt".format(DATE),"a")
+    f.write(a)
+    f.close()
 def Sn(s,r,i,e):
   while s<=e:
    s+=r
    print("U{}={}+{}={}".format(i,s-r,r,s))
+   write("U{}={}+{}={}\n".format(i,s-r,r,s))
    i+=1
 Sn(1800,2790,0,590490)
 s=int(input("Entrez U0:"))
+write("U0={}\n".format(s))
 r=int(input("entrez la raison r:"))
+write("r={}\n".format(r))
 i=0
+write("i={}\n".format(i))
 e=int(input("Entrez la valeur à atteindre:"))
+write("Valeur à atteindre = {}".format(e))
 Sn(s,r,i,e)
 print("créé par Henry letellier")
 Pause=input("Appuyez sur entré pour continuer")
